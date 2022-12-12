24f5ec7585d896109127d0531cb48bdf7023f665
Mikka-t <14maika18@gmail.com>
2022-12-12 17:31:18 +0900

    arrange repository

--- past/ABC101C.py
+++
@@ -1,6 +0,0 @@
-N,K=map(int, input().split())
-A = list(map(int, input().split()))
-
-# how many K-1 in N-1 ?
-ans = -(-(N-1)//(K-1))
-print(ans)
--- practice/ABC095C.py
+++
@@ -1,8 +0,0 @@
-A,B,C,X,Y = map(int, input().split())
-
-money = min(X,Y)*min(A+B, 2*C)
-if X>Y:
-    money += min(A*(X-Y), 2*C*(X-Y))
-else:
-    money += min(B*(Y-X), 2*C*(Y-X))
-print(money)
